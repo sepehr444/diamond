d7105bd08b9642bb50763d1c3f7c3d488d6737bd
sepehr4444 <114873953+sepehr444@users.noreply.github.com>
2024-06-12 20:55:07 +0330

    Delete diamond - Copy directory

--- diamond - Copy/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { GameType } from "../types/gameType";
-export default function Game(canvasId: string, mode?: ("Custom" | "NoMove" | "NoTest" | "NoLoop" | "NoCanvasIntract" | "NoCorner" | "NoJump")[]): GameType;
